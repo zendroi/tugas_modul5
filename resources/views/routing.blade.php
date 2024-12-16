f893687e85397c2fbadc46c6e16d022ecad6d574
zendroi <refykartanegara01@gmail.com>
2024-12-16 16:57:59 +0700

    inisiasi awal

---
+++ resources/views/routing.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial
+scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar Laravel Routing</title>
+    @vite('resources/sass/app.scss')
+</head>
+
+<body>
+    <div class="container m-5">
+        <h1>Belajar Laravel Routing</h1>
+
+        <div class="list-group list-group-numbered mt-4">
+            {{-- Kode anda selanjutnya letakkan di sini --}}
+        </div>
+
+        {{-- Khusus kode program untuk Route Groups di sini --}}
+    </div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\ProfileController; 
+use App\Http\Controllers\EmployeeController; 
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/routing', function() { 
+    return view('routing'); 
+    });
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile'); 
+
+Route::resource('employees', EmployeeController::class); 
